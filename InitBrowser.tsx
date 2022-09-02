9de54d845ad7a58b743d226dcb162ff28262bfe4
Amar Krupalija <amc0kru@gmail.com>
2022-09-02 16:16:51 +0200

    Updated spa-core with MUI5 SSR

--- InitBrowser.tsx
+++ InitBrowser.tsx
@@ -1,54 +1,53 @@
-import ReactDOM from "react-dom";
-import React from "react";
+import ReactDOM from 'react-dom';
+import React from 'react';
 import CmsSite from './Components/CmsSite';
 import AppConfig from './AppConfig';
 import EpiContext from './Spa';
-import ComponentPreLoader, { IComponentPreloadList } from "./Loaders/ComponentPreLoader";
+import ComponentPreLoader, { IComponentPreloadList } from './Loaders/ComponentPreLoader';
 import IServiceContainer from './Core/IServiceContainer';
 import DefaultServiceContainer from './Core/DefaultServiceContainer';
 import ServerContextType from './ServerSideRendering/ServerContext';
+import { CacheProvider } from '@emotion/react';
+import { createMyCache } from './InitServer';
+declare let __INITIAL_DATA__: ServerContextType;
 
-import {
-    StylesProvider,
-    createGenerateClassName,
-  } from "@material-ui/core/styles";
-
-declare let __INITIAL_DATA__ : ServerContextType;
-
-export function InitBrowser(config: AppConfig, containerId?: string, serviceContainer?: IServiceContainer) : void
-{
-    try {
-        if (__INITIAL_DATA__?.status === 'loading') {
-            __INITIAL_DATA__.onReady = () => _doInitBrowser(config, containerId, serviceContainer);
-            return;
-        }
-    } catch (e) {
-        // Ignore on purpose
+export function InitBrowser(config: AppConfig, containerId?: string, serviceContainer?: IServiceContainer): void {
+  try {
+    if (__INITIAL_DATA__?.status === 'loading') {
+      __INITIAL_DATA__.onReady = () => _doInitBrowser(config, containerId, serviceContainer);
+      return;
     }
-    return _doInitBrowser(config, containerId, serviceContainer);
+  } catch (e) {
+    // Ignore on purpose
+  }
+  return _doInitBrowser(config, containerId, serviceContainer);
 }
 
-function _doInitBrowser(config: AppConfig, containerId?: string, serviceContainer?: IServiceContainer) : void
-{
-    const generateClassName = createGenerateClassName({
-        productionPrefix: "MO"
-        //disableGlobal: true
+function _doInitBrowser(config: AppConfig, containerId?: string, serviceContainer?: IServiceContainer): void {
+  EpiContext.init(config, serviceContainer || new DefaultServiceContainer());
+
+  const container = document.getElementById(containerId ? containerId : 'epi-page-container');
+  if (container && container.childElementCount > 0) {
+    const components: IComponentPreloadList = EpiContext.config().preLoadComponents || [];
+    if (EpiContext.isDebugActive())
+      console.info('Hydrating existing render, Stage 1. Preloading components ...', components);
+    const loader = EpiContext.componentLoader();
+
+    const cache = createMyCache();
+
+    ComponentPreLoader.load(components, loader).finally(() => {
+      if (EpiContext.isDebugActive()) console.info('Hydrating existing render, Stage 2. Hydration ...');
+      ReactDOM.hydrate(
+        <CacheProvider value={cache}>
+          <CmsSite context={EpiContext} />
+        </CacheProvider>,
+        container,
+      );
     });
-    EpiContext.init(config, serviceContainer || new DefaultServiceContainer());
-    
-    const container = document.getElementById(containerId ? containerId : "epi-page-container");
-    if (container && container.childElementCount > 0) {
-        const components : IComponentPreloadList = EpiContext.config().preLoadComponents || [];
-        if (EpiContext.isDebugActive()) console.info('Hydrating existing render, Stage 1. Preloading components ...', components);
-        const loader = EpiContext.componentLoader();
-        ComponentPreLoader.load(components, loader).finally(() => {
-            if (EpiContext.isDebugActive()) console.info('Hydrating existing render, Stage 2. Hydration ...');
-            ReactDOM.hydrate(<StylesProvider generateClassName={generateClassName}><CmsSite context={ EpiContext } /></StylesProvider>, container);
-        });
-    } else {
-        if (EpiContext.isDebugActive()) console.info('Building new application');
-        ReactDOM.render(<CmsSite context={ EpiContext } />, container);
-    }
+  } else {
+    if (EpiContext.isDebugActive()) console.info('Building new application');
+    ReactDOM.render(<CmsSite context={EpiContext} />, container);
+  }
 }
 
 export default InitBrowser;
--- InitServer.tsx
+++ InitServer.tsx
@@ -9,51 +9,58 @@ import { StaticRouterContext } from 'react-router';
 
 // Episerver Libraries
 import IServiceContainer from './Core/IServiceContainer';
-import DefaultServiceContainer from './Core/DefaultServiceContainer'; 
+import DefaultServiceContainer from './Core/DefaultServiceContainer';
 import EpiSpaContext from './Spa';
 import CmsSite from './Components/CmsSite';
 import AppConfig from './AppConfig';
-import {
-    ServerStyleSheets,createGenerateClassName
-  } from "@material-ui/core/styles";
+import createCache from '@emotion/cache';
 
-  
+export function createMyCache() {
+  return createCache({ key: 'css' });
+}
+
 // Episerver SPA/PWA Server Side Rendering libs
 import SSRResponse from './ServerSideRendering/Response';
+import createEmotionServer from '@emotion/server/types/create-instance';
+import { CacheProvider } from '@emotion/react';
 
-export default function RenderServerSide(config: AppConfig, serviceContainer?: IServiceContainer): SSRResponse
-{
-    // Update context
-    const ctx = getGlobal();
-    ctx.epi = ctx.epi || {};
-    ctx.epi.isServerSideRendering = true;
+export default function RenderServerSide(config: AppConfig, serviceContainer?: IServiceContainer): SSRResponse {
+  const cache = createMyCache();
+  const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache);
 
-    // Initialize Episerver Context, for Server Side Rendering
-    serviceContainer = serviceContainer || new DefaultServiceContainer();
-    config.enableSpinner = false;
-    config.noAjax = true;
-    config.enableDebug = true;
-    EpiSpaContext.init(config, serviceContainer, true);
-    const classPrefix = "MO";
-    const generateClassName = () => createGenerateClassName({
-        productionPrefix: classPrefix
-    });
+  // Update context
+  const ctx = getGlobal();
+  ctx.epi = ctx.epi || {};
+  ctx.epi.isServerSideRendering = true;
 
-    const staticContext : StaticRouterContext = {};
-    const sheets = new ServerStyleSheets({
-        serverGenerateClassName: generateClassName()
-    });
-    const body = ReactDOMServer.renderToString(sheets.collect(<CmsSite context={ EpiSpaContext } staticContext={ staticContext } />));
-    const meta = Helmet.renderStatic();
+  // Initialize Episerver Context, for Server Side Rendering
+  serviceContainer = serviceContainer || new DefaultServiceContainer();
+  config.enableSpinner = false;
+  config.noAjax = true;
+  config.enableDebug = true;
+  EpiSpaContext.init(config, serviceContainer, true);
+  const classPrefix = 'MO';
 
-    return {
-        Body: body,
-        HtmlAttributes: meta.htmlAttributes.toString(),
-        Title: meta.title.toString(),
-        Meta: meta.meta.toString(),
-        Link: meta.link.toString(),
-        Script: meta.script.toString(),
-        Style: sheets.toString(),
-        BodyAttributes: meta.bodyAttributes.toString()
-    };
+  const staticContext: StaticRouterContext = {};
+
+  const body = ReactDOMServer.renderToString(
+    <CacheProvider value={cache}>
+      <CmsSite context={EpiSpaContext} staticContext={staticContext} />,
+    </CacheProvider>,
+  );
+  const meta = Helmet.renderStatic();
+
+  const emotionChunks = extractCriticalToChunks(body);
+  const emotionCss = constructStyleTagsFromChunks(emotionChunks);
+
+  return {
+    Body: body,
+    HtmlAttributes: meta.htmlAttributes.toString(),
+    Title: meta.title.toString(),
+    Meta: meta.meta.toString(),
+    Link: meta.link.toString(),
+    Script: meta.script.toString(),
+    Style: emotionCss,
+    BodyAttributes: meta.bodyAttributes.toString(),
+  };
 }
